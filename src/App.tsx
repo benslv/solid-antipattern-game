a97d9cb5bb3cb1dc4fedc56d1f0d8b76f8d3b19d
Ben Silverman <bensilverman10@gmail.com>
2025-09-21 16:36:54 +0100

    simplest "AI" xD

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
-import { createMemo, createSignal, Show, type Component } from "solid-js";
+import {
+	createEffect,
+	createMemo,
+	createSignal,
+	Show,
+	type Component,
+} from "solid-js";
 import { checkForPattern, Pebble } from "./util/checkForPattern";
 
 const App: Component = () => {
 	const [state, setState] = createSignal<Array<Pebble>>([]);
+	const [turn, setTurn] = createSignal<1 | 2>(1);
 
 	const appendSymbol = (d: Pebble) => {
 		setState([...state(), d]);
+		setTurn((prev) => (prev === 1 ? 2 : 1));
 	};
 
 	const patternExists = createMemo(() => checkForPattern(state()));
 
+	createEffect(() => {
+		// Computer makes its guess
+		if (turn() === 2) {
+			let pebble: Pebble = Math.random() > 0.5 ? "○" : "●";
+
+			// If the current guess would result in a loss for the computer, switch to the opposite colour.
+			if (checkForPattern([...state(), pebble]).length > 0) {
+				pebble = pebble === "○" ? "●" : "○";
+			}
+
+			appendSymbol(pebble);
+		}
+	});
+
 	return (
 		<>
-			<h1>Hello world!!!!</h1>
+			<h1>Current Turn: {turn()}</h1>
 			<button onClick={() => appendSymbol("○")}>○</button>
 			<button onClick={() => appendSymbol("●")}>●</button>
 			<div>
