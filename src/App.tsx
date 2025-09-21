ff1367aa4e8689d483cd2509fcc823715f917962
Ben Silverman <bensilverman10@gmail.com>
2025-09-21 16:03:38 +0100

    append symbols

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import type { Component } from 'solid-js';
-import Comp from './Comp';
+import { createSignal, type Component } from "solid-js";
+
+const digitMap: Record<0 | 1, string> = {
+	0: "○",
+	1: "●",
+};
 
 const App: Component = () => {
-  return (
-    <>
-      <h1>Hello world!!!!</h1>
-      <Comp />
-    </>
-  );
+	const [state, setState] = createSignal<Array<0 | 1>>([]);
+
+	const appendSymbol = (d: 0 | 1) => {
+		setState([...state(), d]);
+	};
+
+	return (
+		<>
+			<h1>Hello world!!!!</h1>
+			<button onClick={() => appendSymbol(0)}>○</button>
+			<button onClick={() => appendSymbol(1)}>●</button>
+			<div>
+				Pattern:
+				{state()
+					.map((digit) => digitMap[digit])
+					.join("")}
+			</div>
+		</>
+	);
 };
 
 export default App;
